dba571d29ef221a944141660d7fdbd5858650b0b
hamzaadil56 <hamzaadil56@gmail.com>
2023-04-29 13:40:32 +0500

    simplestorage-hardhat

--- stepxx_web3/step05_hardhat/step00_hardhat_token-contract/scripts/deploy.ts
+++ stepxx_web3/step05_hardhat/step00_hardhat_token-contract/scripts/deploy.ts
@@ -9,6 +9,7 @@ async function main() {
 
   const Token = await ethers.getContractFactory("Token");
   const token = await Token.deploy();
+  await token.deployed();
 
   console.log("Token address:", token.address);
 }
---
+++ stepxx_web3/step05_hardhat/step01_hardhat_SimpleStorageContract/hardhat.config.ts
@@ -0,0 +1,38 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
+import "@typechain/hardhat";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+
+dotenv.config();
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL!;
+const PRIVATE_KEY = process.env.PRIVATE_KEY!;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY!;
+const COIN_MARKET_CAP_API = process.env.COIN_MARKET_CAP_API!;
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.8",
+  networks: {
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 11155111,
+    },
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: ETHERSCAN_API_KEY,
+    },
+  },
+  gasReporter: {
+    enabled: true,
+    outputFile: "gas-report.txt",
+    noColors: true,
+    currency: "USD",
+    coinmarketcap: COIN_MARKET_CAP_API,
+  },
+};
+
+export default config;
